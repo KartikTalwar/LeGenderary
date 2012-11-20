8984e2e82458a6f2ac112834472ba2ef96ba47da
Kartik Talwar <hi@kartikt.com>
2012-11-20 09:31:28 -0500

    Added disclaimer in class file

--- leGenderary.py
+++ leGenderary.py
@@ -1,3 +1,18 @@
+"""
+leGenderary.py
+
+Copyright 2012 Kartik Talwar
+
+Licensed under the BSD License
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import os
 import re
 import sys
